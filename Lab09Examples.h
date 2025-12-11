b4beb128a940fca4b43ce28cf669de8d7a529338
sgcs <58474573+sgcs0@users.noreply.github.com>
2025-12-11 15:02:38 +0100

    Add Lab09Examples class with drawing methods

---
+++ Lab09Examples.h
@@ -0,0 +1,16 @@
+#ifndef LAB09EXAMPLES_H
+#define LAB09EXAMPLES_H
+
+class Lab09Examples {
+public:
+    // Przyklad 1 ze strony 8 instrukcji
+    void DrawExample1();
+    
+    // Przyklad 2 (Uklad Sloneczny) ze strony 9
+    void DrawExample2(float alpha, float beta);
+    
+    // Przyklad 3 (Animacja) ze strony 10
+    void DrawExample3(float angle);
+};
+
+#endif
